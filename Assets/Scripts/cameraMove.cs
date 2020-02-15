bc9b943ac5aaca2849b9649d79d7df745cd3e6f4
alikhamesy <alikhamesy0@gmail.com>
2020-02-15 12:30:49 -0800

    things

---
+++ Assets/Scripts/cameraMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMove : MonoBehaviour
+{
+    public GameObject camera;
+    public float xbounds = 8f;
+    public float ybounds = 6f;
+    private Rigidbody2D _rb;
+    private Transform _t;
+    private Rigidbody2D _playerRB;
+    private float _xdif;
+    private float _ydif;    
+    private float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _t = GetComponent<Transform>();
+        _rb = camera.GetComponent<Rigidbody2D>();
+        _speed = GetComponent<move>().speed;
+        _playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _xdif = camera.transform.position.x - _t.position.x;
+        _ydif = camera.transform.position.y - _t.position.y;
+        if((Mathf.Abs(_xdif) > xbounds && _playerRB.velocity.x > 0) || Mathf.Abs(_ydif) > ybounds){
+            _rb.velocity = _playerRB.velocity;
+        }else{
+            _rb.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10f;            
+
+    public Rigidbody2D rb2d;
+
+
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+       
+        float moveVertical = Input.GetAxis("Vertical");
+
+       
+       // Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        
+        rb2d.velocity=new Vector2(moveHorizontal * speed, moveVertical * speed);
+        //rb2d.AddForce(movement * speed);
+    }
+}
