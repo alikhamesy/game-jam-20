3718273cc00327fcf5a6b987f82487a54cef653a
Caleb Whitehead <calebw@sfu.ca>
2020-02-15 17:54:19 -0800

    save scene

--- Assets/Scripts/cameraMove.cs
+++ Assets/Scripts/cameraMove.cs
@@ -25,10 +25,13 @@ public class cameraMove : MonoBehaviour
     {
         _xdif = _t.position.x - camera.transform.position.x;
         _ydif = _t.position.y - camera.transform.position.y;
-        if((Mathf.Abs(_xdif) > xbounds && ((_xdif > 0 && _playerRB.velocity.x > 0) || (_xdif < 0 && _playerRB.velocity.x < 0)))
-          || (Mathf.Abs(_ydif) > ybounds && ((_ydif > 0 && _playerRB.velocity.y > 0) || (_ydif < 0 && _playerRB.velocity.y < 0)))){
+        if ((Mathf.Abs(_xdif) > xbounds && ((_xdif > 0 && _playerRB.velocity.x > 0) || (_xdif < 0 && _playerRB.velocity.x < 0)))
+          || (Mathf.Abs(_ydif) > ybounds && ((_ydif > 0 && _playerRB.velocity.y > 0) || (_ydif < 0 && _playerRB.velocity.y < 0))))
+        {
             _rb.velocity = _playerRB.velocity;
-        }else{
+        }
+        else
+        {
             _rb.velocity = new Vector2(0f, 0f);
         }
     }
